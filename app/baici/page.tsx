6c6b5e08c08a7a7e9c5dbc0a8df9e41a43bb92fd
Narix Hine <127665924+NarixHine@users.noreply.github.com>
2023-12-03 00:40:10 +0800

    `/baici` Implemented

---
+++ app/baici/page.tsx
@@ -0,0 +1,68 @@
+import Image from 'next/image'
+import Lattice from './lattice.png'
+import Snow from './bg.png'
+import Logo from './logo.png'
+import Fade from '@/components/fade'
+
+export default function China() {
+    return (<main>
+        <div className='relative h-screen w-full'>
+            <Image
+                src={Lattice}
+                alt='Lattice'
+                fill={true}
+                className='overflow-hidden object-cover object-center'
+            ></Image>
+            <Image alt='Logo' width={600} src={Logo} className='p-5 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'></Image>
+            <video className='object-cover h-full w-full' autoPlay loop muted>
+                <source src={'/garden.mp4'} type='video/mp4' />
+            </video>
+        </div>
+        <div
+            className='from-slate-100 from-20% p-10 w-full to-black to-70% bg-gradient-to-b border-b-2 border-slate-300'
+        >
+            <br></br>
+            <div className='mx-auto flex w-full space-x-4 text-blue-700'>
+                <div className='flex-1 text-right text-xl lg:text-4xl'><Fade>宋神宗即位，变法。</Fade></div>
+                <div className='flex-row justify-items-center opacity-70'>
+                    <div className='w-4 h-4 rounded-full bg-blue-700 my-3'></div>
+                    <div className='w-1 h-40 rounded-sm mx-auto bg-gradient-to-b from-blue-700 to-blue-500'></div>
+                </div>
+                <div className='flex-1 text-left text-md lg:text-xl'>1067年</div>
+            </div>
+            <div className='mx-auto flex w-full space-x-4 text-blue-500'>
+                <div className='flex-1 text-right text-md lg:text-xl'>1069年</div>
+                <div className='flex-row justify-items-center opacity-70'>
+                    <div className='w-4 h-4 rounded-full bg-blue-500 my-3'></div>
+                    <div className='w-1 h-40 rounded-sm mx-auto bg-gradient-to-b from-blue-500 to-blue-300'></div>
+                </div>
+                <div className='flex-1 text-left text-xl lg:text-4xl'><Fade>任用王安石为参知政事。</Fade></div>
+            </div>
+            <div className='mx-auto flex w-full space-x-4 text-blue-300'>
+                <div className='flex-1 text-right text-xl lg:text-4xl'><Fade>宋神宗忧忿而死。</Fade></div>
+                <div className='flex-row justify-items-center opacity-70'>
+                    <div className='w-4 h-4 rounded-full bg-blue-300 my-3'></div>
+                    <div className='w-1 h-60 rounded-sm mx-auto bg-gradient-to-b from-blue-300 to-blue-100'></div>
+                </div>
+                <div className='flex-1 text-left text-md lg:text-xl'>1085年</div>
+            </div>
+            <div className='text-4xl lg:text-6xl text-center text-blue-100 my-16'><Fade>靖康之变，白瓷流落民间。</Fade></div>
+            <div className='text-4xl lg:text-6xl text-center text-blue-100 my-24 opacity-70'><Fade>🡫</Fade></div>
+
+        </div>
+        <div className='relative w-full p-3'>
+            <Image
+                src={Snow}
+                alt='Snowy Town'
+                fill={true}
+                className='overflow-hidden object-cover object-center -z-10'
+            ></Image>
+            <Fade>
+                <div className='text-center text-4xl font-black my-6'>观看完整视频。</div>
+                <video controls width={'60%'} className='aspect-video mx-auto my-10'>
+                    <source src={'/garden.mp4'} type='video/mp4' />
+                </video>
+            </Fade>
+        </div>
+    </main >)
+}
---
+++ app/navbar.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link'
+import { FcEditImage } from 'react-icons/fc'
+import { FcFilm } from 'react-icons/fc'
+import { FcLandscape } from 'react-icons/fc'
+
+function NavBar() {
+    return (
+        <nav
+            className={'flex w-full z-50 items-center space-x-4 lg:space-x-6 p-4 fixed top-0 border-y-amber-950 bg-slate-200/20 border-y-2'}
+        >
+            <h1 className='font-bold text-xl text-transparent bg-clip-text bg-gradient-to-br from-amber-400 to-blue-400'>
+                <Link href={''}>非物质的永恒</Link>
+            </h1>
+            <span className='flex-1'></span>
+            <Link
+                href=''
+                className='text-lg transition-colors hover:text-primary'
+            >
+                <FcEditImage></FcEditImage>
+            </Link>
+            <Link
+                href=''
+                className='text-lg text-muted-foreground transition-colors hover:text-primary'
+            >
+                <FcLandscape></FcLandscape>
+            </Link>
+            <Link
+                href=''
+                className='text-lg text-muted-foreground transition-colors hover:text-primary'
+            >
+                <FcFilm></FcFilm>
+            </Link>
+        </nav>
+    )
+}
+
+export default NavBar
